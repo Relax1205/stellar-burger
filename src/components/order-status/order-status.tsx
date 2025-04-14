6ff91279968a9b332ee3fb1f33e2d7cea4162d91
Relax <nikitafon2005@gmail.com>
2025-04-14 22:50:54 +0300

    add routing and e.t.c

--- src/components/order-status/order-status.tsx
+++ src/components/order-status/order-status.tsx
@@ -1,25 +1,18 @@
-import React, { FC } from 'react';
-import { OrderStatusProps } from './type';
-import { OrderStatusUI } from '@ui';
+import { FC } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { ProfileMenuUI } from '@ui';
+import { useDispatch } from '@store';
+import { logoutUser } from '../../services/slices/userSlice/userSlice';
 
-const statusText: { [key: string]: string } = {
-  pending: 'Готовится',
-  done: 'Выполнен',
-  created: 'Создан'
-};
+export const ProfileMenu: FC = () => {
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export const OrderStatus: FC<OrderStatusProps> = ({ status }) => {
-  let textStyle = '';
-  switch (status) {
-    case 'pending':
-      textStyle = '#E52B1A';
-      break;
-    case 'done':
-      textStyle = '#00CCCC';
-      break;
-    default:
-      textStyle = '#F2F2F3';
-  }
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    navigate('/');
+  };
 
-  return <OrderStatusUI textStyle={textStyle} text={statusText[textStyle]} />;
+  return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
